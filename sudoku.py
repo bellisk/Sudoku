c7ba2107f6f2ce3ef5328233431f0109e877448a
bellisk <daisywheel22@gmail.com>
2013-01-27 18:17:09 +0100

    Condensed the code.

--- sudoku.py
+++ sudoku.py
@@ -4,87 +4,59 @@ def parse(text):
 def emit(grid):
 	return "\n".join(["".join([str(c) for c in row]) for row in grid])
 
+def get_row(grid, row_index):
+	return grid[row_index]
+
+def get_column(grid, column_index):
+	return [row[column_index] for row in grid]
+
+def get_subgrid(grid, subgrid_row_index, subgrid_column_index):
+	subgrid = []
+	for y in range(3*subgrid_row_index, 3*subgrid_row_index + 3):
+		for x in range(3*subgrid_column_index, 3*subgrid_column_index + 3):
+			subgrid.append(grid[y][x])
+	return subgrid
+
+def valid(shape):
+	return all([c == 0 or shape.count(c) == 1 for c in shape])
+
+def complete(shape):
+	return not 0 in shape
+
 def valid_row(grid, row_index):
-	for c in grid[row_index]:
-		if c != 0:
-			if grid[row_index].count(c) > 1:
-				return False
-	return True
+	return valid(get_row(grid, row_index))
 
 def valid_column(grid, column_index):
-	column = []
-	for x in range (9):
-		column.append(grid[x][column_index])
-	for c in column:
-		if c != 0:
-			if column.count(c) > 1:
-				return False
-	return True
+	return valid(get_column(grid, column_index))
 
 def complete_row(grid, row_index):
-	if len(grid[row_index]) != 9:
-		return False
-	if 0 in grid[row_index]:
-		return False
-	return True
+	return complete(get_row(grid, row_index))
 
 def complete_column(grid, column_index):
-	column = []
-	for x in range (9):
-		column.append(grid[x][column_index])
-	if len(column) != 9:
-		return False
-	if 0 in column:
-		return False
-	return True
+	return complete(get_column(grid, column_index))
 
 def valid_subgrid(grid, subgrid_row_index, subgrid_column_index):
-	subgrid = []
-	for y in range(3*subgrid_row_index, 3*subgrid_row_index + 3):
-		for x in range(3*subgrid_column_index, 3*subgrid_column_index + 3):
-			subgrid.append(grid[y][x])
-	for c in subgrid:
-		if c != 0:
-			if subgrid.count(c) > 1:
-				return False
-	return True
+	return valid(get_subgrid(grid, subgrid_row_index, subgrid_column_index))
 
 def complete_subgrid(grid, subgrid_row_index, subgrid_column_index):
-	subgrid = []
-	for y in range(3*subgrid_row_index, 3*subgrid_row_index + 3):
-		for x in range(3*subgrid_column_index, 3*subgrid_column_index + 3):
-			subgrid.append(grid[y][x])
-	if len(subgrid) != 9:
-		return False
-	if 0 in subgrid:
-		return False
-	return True
+	return complete(get_subgrid(grid, subgrid_row_index, subgrid_column_index))
+
+def get_shapes(grid):
+	shapes = []
+	for y in range(9):
+		shapes.append(get_row(grid, y))
+	for x in range(9):
+		shapes.append(get_column(grid, x))
+	for y in range(3):
+		for x in range(3):
+			shapes.append(get_subgrid(grid, y, x))
+	return shapes
 
 def valid_grid(grid):
-	for y in range(9):
-		if not valid_row(grid, y):
-			return False
-	for x in range(9):
-		if not valid_column(grid, x):
-			return False
-	for y in range(3):
-		for x in range(3):
-			if not valid_subgrid(grid, y, x):
-				return False
-	return True
+	return all([valid(s) for s in get_shapes(grid)])
 
 def complete_grid(grid):
-	for y in range(9):
-		if not complete_row(grid, y):
-			return False
-	for x in range(9):
-		if not complete_column(grid, x):
-			return False
-	for y in range(3):
-		for x in range(3):
-			if not complete_subgrid(grid, y, x):
-				return False
-	return True
+	return all([complete(s) for s in get_shapes(grid)])
 
 def solved_grid(grid):
 	return valid_grid(grid) and complete_grid(grid)
